86ce81d60fc2dee49c2d9f5cff87c9dec6d8d6d7
Danilo Caldas <danilo-caldas-0000@hotmail.com>
2019-10-25 00:44:40 -0200

    Classes POJO e DAO criadas

--- programadores/src/main/java/br/ufma/ecp/programadores/Programador.java
+++ programadores/src/main/java/br/ufma/ecp/programadores/Programador.java
@@ -1,5 +1,35 @@
 package br.ufma.ecp.programadores;
 
 public class Programador {
-
+	
+	private String nome;
+	private String email;
+	private String celular;
+	
+	public Programador(String nome, String email, String celular) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.celular = celular;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
 }
--- programadores/src/main/java/br/ufma/ecp/programadores/ProgramadorDAO.java
+++ programadores/src/main/java/br/ufma/ecp/programadores/ProgramadorDAO.java
@@ -1,5 +1,42 @@
 package br.ufma.ecp.programadores;
 
-public class ProgramadorDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class ProgramadorDAO {
+	
+	private Connection conn;
+	private String url;
+
+	public ProgramadorDAO(String username, String password) {
+		
+		url = "jdbc:mysql://localhost/estante?user=" + username + "&password=" + password + "&useTimezone=true&serverTimezone=UTC";
+	
+	}
+	
+	public void create (Programador programador) throws SQLException {
+		
+		String sql = "insert into programador (nome, email, celular) values (?,?,?)";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		
+		stmt.setString(1, programador.getNome());
+		stmt.setString(2, programador.getEmail());
+		stmt.setString(3, programador.getCelular());
+		
+		stmt.execute();
+		stmt.close();
+		
+	}
+	
+	public void openConnection () throws SQLException {
+		conn = DriverManager.getConnection(url);
+	}
+	
+	public void closeConnection () throws SQLException {
+		conn.close();
+	}
+	
 }
